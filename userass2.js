69a36caaeec3156dea2c8d5f7d1399980f83c0c7
abhi78945 <52705278+abhi78945@users.noreply.github.com>
2020-07-23 12:13:05 +0530

    assignment 2 , question 1 and 3

---
+++ userass2.js
@@ -0,0 +1,10 @@
+let age = Number(prompt("Enter your age"));
+ //{
+ //console.log(age, typeof age);
+ //}
+ if(age >21){
+        console.log("Can drink")
+     }
+     else{
+       console.log("cannot drink");
+     }
